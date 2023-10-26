ce5218c11444c3c8b7919bf9b989147a2b025e1f
iratze <iratze.eizaguirre@gmail.com>
2023-10-26 19:07:53 +0100

    Animation list added

---
+++ animationFilms.js
@@ -0,0 +1,31 @@
+const AnimationFilms = [
+  "El rey león (1994)",
+  "Your Name (2016)",
+  "Mi vecino Totoro (1988)",
+  "Wall-E (2008)",
+  "La princesa Mononoke (1997)",
+  "Coco (2017)",
+  "El viaje de Chihiro (2001)",
+  "Toy Story (1995)",
+  "Frozen (2013)",
+  "Shrek (2001)",
+  "Pesadilla antes de Navidad (1993)",
+  "Intensa-mente (Inside Out) (2015)",
+  "Los increíbles (2004)",
+  "Cómo entrenar a tu dragón (2010)",
+  "Kung Fu Panda (2008)",
+  "Spiderman: A través del spider-verso (2018)",
+  "El niño y la bestia (2015)",
+  "Fantasía (1940)",
+  "El castillo ambulante (2004)",
+  "El jardín secreto (Arrietty) (2010)",
+  "Up (2009)",
+  "Monstruos, S.A. (2001)",
+  "El castillo en el cielo (1986)",
+  "Los mundos de Coraline (2009)",
+  "Buscando a Nemo (2003)"
+];
+
+console.log(animationFilms);
+
+module.exports = animationFilms;
